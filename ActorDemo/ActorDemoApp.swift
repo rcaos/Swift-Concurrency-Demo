4fbb6094541976583dd6b3c9351f9b3b76d12352
Jeans <jeansruiz.c@gmail.com>
2023-08-21 16:53:16 -0500

    Initial Commit

---
+++ ActorDemo/ActorDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ActorDemoApp.swift
+//  ActorDemo
+//
+//  Created by Jeans Ruiz on 21/08/23.
+//
+
+import SwiftUI
+
+@main
+struct ActorDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
