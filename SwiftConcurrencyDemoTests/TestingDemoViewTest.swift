1a92a795d5356d6d18c7e776d2d50acaf58dc40d
Jeans <jeansruiz.c@gmail.com>
2023-09-04 09:52:31 -0500

    override convenience method

--- SwiftConcurrencyDemoTests/TestingDemoViewTest.swift
+++ SwiftConcurrencyDemoTests/TestingDemoViewTest.swift
@@ -8,25 +8,29 @@ import ConcurrencyExtras
 
 final class TestingDemoViewTest: XCTestCase {
 
-  func testExample() async {
-    await withMainSerialExecutor {
-      let mock = FetchRandomUser(execute: { request in
-        //await Task.yield() /// It's mandatory?
-        return .init(id: 1, name: "name", email: "aa@mail")
-      })
-
-      let sut = TestingDemoModel(fetchRandomUser: { mock })
-
-      let task = Task {
-        await sut.getRandomUser()
-      }
-      await Task.yield()
-      XCTAssertTrue(sut.isRequestingUser)
-      await sut.userTask?.value
-      await task.value
-
-      XCTAssertEqual(sut.user, .init(id: 1, name: "name", email: "aa@mail"))
-      XCTAssertFalse(sut.isRequestingUser)
+  override func invokeTest() {
+    withMainSerialExecutor {
+      super.invokeTest()
     }
+  }
+
+  func testExample() async {
+    let mock = FetchRandomUser(execute: { request in
+      //await Task.yield() /// It's mandatory?
+      return .init(id: 1, name: "name", email: "aa@mail")
+    })
+
+    let sut = TestingDemoModel(fetchRandomUser: { mock })
+
+    let task = Task {
+      await sut.getRandomUser()
+    }
+    await Task.yield()
+    XCTAssertTrue(sut.isRequestingUser)
+    await sut.userTask?.value
+    await task.value
+
+    XCTAssertEqual(sut.user, .init(id: 1, name: "name", email: "aa@mail"))
+    XCTAssertFalse(sut.isRequestingUser)
   }
 }
