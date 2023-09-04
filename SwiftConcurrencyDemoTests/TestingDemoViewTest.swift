e81755e1844d1b0241cd8e186ab7dfc6a931e67f
Jeans <jeansruiz.c@gmail.com>
2023-09-04 09:50:58 -0500

    Add test

---
+++ SwiftConcurrencyDemoTests/TestingDemoViewTest.swift
@@ -0,0 +1,32 @@
+//
+//  Created by Jeans Ruiz on 1/09/23.
+//
+
+import XCTest
+import ConcurrencyExtras
+@testable import SwiftConcurrencyDemo
+
+final class TestingDemoViewTest: XCTestCase {
+
+  func testExample() async {
+    await withMainSerialExecutor {
+      let mock = FetchRandomUser(execute: { request in
+        //await Task.yield() /// It's mandatory?
+        return .init(id: 1, name: "name", email: "aa@mail")
+      })
+
+      let sut = TestingDemoModel(fetchRandomUser: { mock })
+
+      let task = Task {
+        await sut.getRandomUser()
+      }
+      await Task.yield()
+      XCTAssertTrue(sut.isRequestingUser)
+      await sut.userTask?.value
+      await task.value
+
+      XCTAssertEqual(sut.user, .init(id: 1, name: "name", email: "aa@mail"))
+      XCTAssertFalse(sut.isRequestingUser)
+    }
+  }
+}
